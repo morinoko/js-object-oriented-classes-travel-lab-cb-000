4cd4064caf78c95c4d99fa6f18150605895b0e56
Felice Forby <feliceforby@gmail.com>
2019-02-07 23:35:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
+const eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    const endDate = new Date(year, 1, 1);
+    const startYear = this.startDate.getFullYear();
+    const endYear = endDate.getFullYear();
+
+    return endYear - startYear;
+  }
+}
+
+class Route {
+  // Locations are objecst with horizontal and vertical attributes:
+  // { horizontal: 'Park', vertical: '34' }
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  avenueToInteger(avenue) {
+    return eastWest.indexOf(avenue) + 1;
+  }
+
+  blocksTravelled() {
+    const horizontalBlocksTravelled =
+      Math.abs(this.avenueToInteger(this.endingLocation.horizontal) -
+      this.avenueToInteger(this.beginningLocation.horizontal));
+
+    const verticalBlocksTravelled =
+      Math.abs(this.endingLocation.vertical - 
+      this.beginningLocation.vertical);
+
+    return horizontalBlocksTravelled + verticalBlocksTravelled;
+  }
+
+
+  estimatedTime(peakHours = false) {
+    const blocksPerMinute = peakHours ? 2 : 3;
+
+    return this.blocksTravelled() / blocksPerMinute;
+  }
+}
